4f40670d5591bbce1ac7ae6da539a8644cc5f319
aryat <aryatsrivastava06@gmail.com>
2026-02-21 20:56:43 +0530

    Removed unncecessary code

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { Text, TextInput, View } from 'react-native';
+
+export const TorchHomeScreen = () => {
+	return (
+		<View>
+		</View>
+	)
+};
+
+
